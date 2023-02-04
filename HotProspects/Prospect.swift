ffbbb2dbd62a921da114f1c860b123ccd952f7c1
Konneki <konnekdaniel@gmail.com>
2023-02-04 22:27:58 +0100

    Making layout using tabview

---
+++ HotProspects/Prospect.swift
@@ -0,0 +1,8 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Daniel Konnek on 4.02.2023.
+//
+
+import SwiftUI
---
+++ HotProspects/ProspectsView.swift
@@ -0,0 +1,40 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by Daniel Konnek on 4.02.2023.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    enum FilterType {
+        case none, contacted, uncontacted
+    }
+    
+    let filter: FilterType
+    
+    var title: String {
+        switch filter {
+        case .none:
+            return "Everyone"
+        case .contacted:
+            return "Contacted people"
+        case .uncontacted:
+            return "Uncontacted people"
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle(title)
+        }
+    }
+}
+
+struct ProspectsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProspectsView(filter: .none)
+    }
+}
